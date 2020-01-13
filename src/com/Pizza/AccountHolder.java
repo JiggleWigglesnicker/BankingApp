e059c204ba2796d0891eda4bfa9b414f239b1eb9
Snicker <games.j@live.nl>
2020-01-13 20:25:03 +0100

    added in new methode account holder

--- src/com/Pizza/AccountHolder.java
+++ src/com/Pizza/AccountHolder.java
@@ -1,10 +1,14 @@
 package com.Pizza;
 
+import java.util.HashMap;
 import java.util.HashSet;
+import java.util.Map;
 
 public class AccountHolder {
     private String nameHolder;
     private HashSet<Account> accountList = new HashSet<Account>();
+    private Double totalHolderAmount = 0.0;
+    private HashMap<String, Double> totalCurrencyList = new HashMap<String, Double>();
 
     public AccountHolder(String name){
         nameHolder = name;
@@ -24,5 +28,45 @@ public class AccountHolder {
 
     public double accountCount(){
         return accountList.size();
+    }
+
+    public void addUpIndividualAccountCurrencies(){
+        HashMap<String, Double> prevAmount = new HashMap<String,Double>();
+
+        for(Account account : accountList){
+            for(Currency currency : account.getMultiCurrenciesList()){
+                String nameCurrency = currency.getCurrencyName();
+                Double amountCurrency = currency.getAmount();
+
+                prevAmount.put(nameCurrency, amountCurrency);
+            }
+
+            if(totalCurrencyList.isEmpty()) {
+                for(Map.Entry<String,Double> entry : prevAmount.entrySet()){
+                    totalCurrencyList.put(entry.getKey(),entry.getValue());
+                }
+            }else{
+                for(Map.Entry<String,Double> entry : prevAmount.entrySet()){
+                    totalCurrencyList.put(entry.getKey(),totalCurrencyList.get(entry.getKey()) +entry.getValue() );
+                }
+            }
+        }
+
+    }
+
+    public void calcTotalAmountHolder(){
+        for(Account account : accountList){
+            for(Currency currency : account.getMultiCurrenciesList()){
+                for(Map.Entry<String,Double> entry : totalCurrencyList.entrySet()){
+
+                }
+            }
+
+        }
+    }
+
+
+    public HashMap<String,Double> getTotalCurrencyList(){
+        return totalCurrencyList;
     }
 }
