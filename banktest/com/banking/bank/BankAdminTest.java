a085cdc44161ebb0aaae02aa17c1393c6afdbc3b
Snicker <games.j@live.nl>
2020-01-19 20:30:37 +0100

    added unit test and deleted old ones

--- banktest/com/banking/bank/BankAdminTest.java
+++ banktest/com/banking/bank/BankAdminTest.java
@@ -6,14 +6,17 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class BankAdminTest {
 
+    //test if total amount that the bank has is returned in the desired currency type
     @Test
     void calcBankTotal() {
     }
 
+    // test if the correct account is returned when looking for the poorest account holder
     @Test
     void poorestAccount() {
     }
 
+    // test if the correct account is returned when looking for the richest account holder
     @Test
     void richestAccount() {
     }
