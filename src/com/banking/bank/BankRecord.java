84e5cd553332a485c88e70e86a29aeaaf7f210a7
Snicker <games.j@live.nl>
2020-01-19 20:18:07 +0100

    added unit test and deleted old ones

--- src/com/banking/bank/BankRecord.java
+++ src/com/banking/bank/BankRecord.java
@@ -5,26 +5,31 @@ import java.util.HashSet;
 import java.util.Map;
 
 public class BankRecord {
-
+    // holds HashSet of accountHolders
     private HashSet<AccountHolder> accountHolderList = new HashSet<AccountHolder>();
+    //Holds the total amount of each currency type which is stored in the account of the holders
     private HashMap<String, Double> recordTotalIndividualCurrencies = new HashMap<String, Double>();
 
     public BankRecord() {
 
     }
 
+    // gets accountholder Hashset
     public HashSet<AccountHolder> getAccountHolderList() {
         return accountHolderList;
     }
 
+    //adds account holder to HashSet accountHolder list
     public void addHolder(AccountHolder holder) {
         accountHolderList.add(holder);
     }
 
+    //gets the total amount of each currency type which is stored in the account of the holders
     public HashMap<String, Double> getRecordTotalIndividualCurrencies() {
         return recordTotalIndividualCurrencies;
     }
 
+    //calculates the total amount of different currencies stored in the account of the account holders separating them through currency type
     public void calcRecordTotalIndividualCurrencies() {
         for (AccountHolder holder : accountHolderList) {
             for (Map.Entry<String, Double> entry : holder.getTotalCurrencyList().entrySet()) {
--- src/com/banking/currency/CryptoRegulator.java
+++ src/com/banking/currency/CryptoRegulator.java
@@ -1,6 +1,6 @@
 package com.banking.currency;
 
-//TODO: doe er wat mee, zet dit limiet "hard" in de bitcoin/ethereum
+
 public interface CryptoRegulator {
     Double getCryptoLimit();
 }
