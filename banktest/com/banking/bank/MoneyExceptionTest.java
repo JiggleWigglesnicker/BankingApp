4983435e082d72b054354f4bf1b5429df2e5be28
Snicker <games.j@live.nl>
2020-01-20 15:01:14 +0100

    added unit test and deleted old ones

--- banktest/com/banking/bank/MoneyExceptionTest.java
+++ banktest/com/banking/bank/MoneyExceptionTest.java
@@ -1,7 +1,27 @@
 package com.banking.bank;
 
-import static org.junit.jupiter.api.Assertions.*;
+import com.banking.currency.Euro;
+import com.banking.currency.Yen;
+import com.banking.exception.MoneyException;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class MoneyExceptionTest {
+
+    // If not enough funds are available for conversion throws MoneyException
+    @Test
+    void testMoneyException(){
+        Assertions.assertThrows(MoneyException.class, () -> {
+            Euro euro1 = new Euro(3.0);
+            Yen yen1 = new Yen(230.0);
+            Account account1 = new Account("TrustFund");
+            account1.addCurrency(euro1);
+            account1.addCurrency(yen1);
+            account1.conversion(euro1, yen1, 15.0);
+        });
+
+    }
 
 }
